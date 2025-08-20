2b7a4a92744741fae0a77b557864daf4966995b5
reyna <171039150+reynabxr@users.noreply.github.com>
2025-08-20 18:06:19 +0800

    initial skeletal version

---
+++ src/main/java/Kane.java
@@ -0,0 +1,6 @@
+public class Kane {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Kane\n" + "What can I do for you?\n"
+        + "Bye! Hope to see you soon!");
+    }
+}
