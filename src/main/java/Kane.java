eda3a8197063012e3a737b0669d35639928b806a
reyna <171039150+reynabxr@users.noreply.github.com>
2025-08-21 17:47:07 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Kane.java
+++ src/main/java/Kane.java
@@ -1,15 +1,27 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Kane {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
+        int end = 0;
 
         System.out.println("Hello! I'm Kane.\n" + "What can I do for you?\n");
         String input = sc.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println(input);
-            input = sc.nextLine();
+            if (input.equals("list")) {
+                for (int i = 0; i < end; i++) {
+                    System.out.println(i + 1 + ". " + tasks.get(i));
+                }
+                input = sc.nextLine();
+            } else {
+                tasks.add(input);
+                end++;
+                System.out.println("added: " + input);
+                input = sc.nextLine();
+            }
         }
         System.out.println("Bye! Hope to see you soon!");
         sc.close();
