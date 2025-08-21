f9a323b838700321a27dbe4468f99ba3f91db5cc
reyna <171039150+reynabxr@users.noreply.github.com>
2025-08-21 17:26:16 +0800

    Improved skeletal version such that it echos commands entered by the user and exits when the user types the command bye

--- src/main/java/Kane.java
+++ src/main/java/Kane.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Kane {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Kane\n" + "What can I do for you?\n"
-        + "Bye! Hope to see you soon!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello! I'm Kane.\n" + "What can I do for you?\n");
+        String input = sc.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+        System.out.println("Bye! Hope to see you soon!");
+        sc.close();
     }
 }
