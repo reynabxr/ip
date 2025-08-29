4d085ece86db36dd0664cf2cc9664aa4b68f176a
reyna <171039150+reynabxr@users.noreply.github.com>
2025-08-29 10:58:06 +0800

    Add dates and times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,20 +1,29 @@
-public class Deadline extends Task {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    protected String by;
+public class Deadline extends Task {
+    protected LocalDateTime by;
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mm a");
 
     public Deadline(String description, String by) {
         super(description, TaskType.DEADLINE);
-        this.by = by;
+        try {
+            this.by = LocalDateTime.parse(by, INPUT_FORMAT);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("\"Invalid date format! Please use yyyy-MM-dd HHmm, e.g. 2019-12-02 1800.");
+        }
     }
 
     @Override
     public String toString() {
-        return super.toString() + " (by: " + by + ")";
+        return super.toString() + " (by: " + this.by.format(OUTPUT_FORMAT) + ")";
     }
 
     @Override
     public String toFileString() {
-        return super.toFileString() + " | " + by;
+        return super.toFileString() + " | " + by.format(INPUT_FORMAT);
     }
 }
 
